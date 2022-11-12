42355cf226be1fa42770967cd89bd8ab9e59c676
Touseef Liaqat <touseefliaqat@gmail.com>
2022-11-11 17:37:25 -0800

    Add embeddable editor that works with teachable
    We can add this code in each problem lesson. 
    This embeddable will show two tabs (Editor, and Solution)
    Users can code in the editor. And check the provided solution from the Solution tab.

---
+++ editor.js
@@ -0,0 +1,134 @@
+
+<style type="text/css" media="screen">
+    #editor { 
+      width: 100%;
+      height: 100%;
+      border-bottom-left-radius: 8px;
+      border-bottom-right-radius: 8px;
+    }
+    
+    .editor-container { 
+      width: 100%;
+      height: 500px;
+      padding-bottom: 40px;
+    }
+    
+    .editor-tabs {
+      border-top-left-radius: 8px;
+      border-top-right-radius: 8px;
+      padding: 0px;
+      margin: 0;
+    		display: flex;
+    		background: #2f3240;
+        color: #f8f8f2;
+      	width: 100%;
+        height: 60px;
+        font: 12px/normal 'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace;
+    }
+    
+    .editor-tab {
+      
+      position: relative;
+      appearance: none;
+      list-style: none;
+      background: transparent;
+      padding: 1em 0;
+      border: none;
+      font: inherit;
+      
+      cursor: pointer;
+      outline: none;
+      flex-grow: 1;
+      text-align: center;
+      transition: color .3s ease;
+      z-index: 1;
+    }
+    
+    .editor-tab--active {
+      border-top-left-radius: 8px;
+      border-top-right-radius: 8px;
+      background-color: #3c3f52;
+    }
+    
+    .editor-tab--right-border {
+      border-right-color: #121319;
+      border-right-width: 1px;
+      border-right-style: solid;
+    }
+    
+    .editor-fullscreen-closer {
+      z-index: 20;
+      position: absolute;
+      right: 5px;
+      bottom: 5px;
+      background: none;
+      border: none;
+      cursor: pointer;
+    }
+    
+    #editor-fullscreen-closer__icon {
+      fill: #f8f8f2;
+    }
+
+</style>
+
+<div class="editor-container">
+    <ul class="editor-tabs">
+      <li id="edit-js" class="editor-tab editor-tab--right-border editor-tab--active">Component</li>
+      <li id="edit-css" class="editor-tab editor-tab--right-border">Solution</li>
+      
+    </ul>
+    <div id="editor" class="editor active">
+    </div>
+    <button class="editor-fullscreen-closer"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path id="editor-fullscreen-closer__icon" d="M4.5 11H3v4h4v-1.5H4.5V11zM3 7h1.5V4.5H7V3H3v4zm10.5 6.5H11V15h4v-4h-1.5v2.5zM11 3v1.5h2.5V7H15V3h-4z"/></svg></button>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.13.0/ace.js" type="text/javascript" charset="utf-8"></script>
+<script>
+    
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    
+    var js = new ace.EditSession(`def findAverage(nums: List[int], k: int) -> float:
+    # Your code...`);
+    js.setMode('ace/mode/python');
+    var css = new ace.EditSession(`def findAverage(nums: List[int], k: int) -> float:
+        
+        windowSum = sum(nums[:k])
+        average = windowSum/k
+        result = [average]
+        
+        for i in range(len(nums)-k):
+            windowSum = windowSum - nums[i] + nums[i+k]
+            average = windowSum/k
+            result.append(average)
+            
+        return result
+    `);
+    css.setMode('ace/mode/python');
+
+    editor.setTheme("ace/theme/dracula");
+    editor.setSession(js);
+    
+    const codeTab = document.getElementById('edit-js');
+    const cssTab = document.getElementById('edit-css');
+    
+    const selectTab = function (tabId) {
+        const lastSelectedTab = document.getElementsByClassName('editor-tab--active');
+      for (let i = 0; i < lastSelectedTab.length; i += 1) {
+        lastSelectedTab[i].classList.remove('editor-tab--active');
+      }
+      const tab = document.getElementById(tabId);
+      tab.classList.add('editor-tab--active');
+    }
+    
+    codeTab.addEventListener('click', () => {
+      selectTab('edit-js');
+      editor.setSession(js);
+    });
+    cssTab.addEventListener('click', () => {
+      selectTab('edit-css');
+      editor.setSession(css);
+    });
+    
+</script>
