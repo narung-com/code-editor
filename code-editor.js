f0d34868e1730449cafea736dcb21ceb014f0384
Rabi Siddique <60459172+rabi-siddique@users.noreply.github.com>
2022-11-17 19:00:25 +0500

    cleaned-up code

--- code-editor.js
+++ code-editor.js
@@ -1,10 +1,7 @@
-
-  !function(){
-    console.log("In function");
+(function(){
     var editorContainer = document.getElementById('editor-container');
     var problem = editorContainer.getAttribute('problem');
     localStorage['problem'] = problem;
-    console.log(problem);
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     
@@ -19,19 +16,16 @@
     var solutions = null;
     
     async function fetchCode() {
-    
-      id = String(_user_id) + lecture_data.lectureId;
-      response = await getCode(id);
+      var id = String(_user_id) + lecture_data.lectureId;
+      var response = await getCode(id);
     
       js.setValue(response)
     
-      fetchAnswer("Python3").then((resp) => {
-        console.log("inside fetchAnswer" + JSON.stringify(resp));
-        localStorage[localStorage['problem']] = JSON.stringify(resp);
-        
-        css.setValue(JSON.parse(localStorage[localStorage['problem']])["Python3"]);
-      });
+      var response = fetchAnswer("Python3")
+      localStorage[localStorage['problem']] = JSON.stringify(response);
+      css.setValue(JSON.parse(localStorage[localStorage['problem']])["Python3"]);
     }
+
     window.onload = async function(){
       var editorContainer = document.getElementById('editor-container');
       localStorage['problem'] = editorContainer.getAttribute('problem');
@@ -46,7 +40,7 @@
     });
       
     
-    const saveCode = async function (id, code) {
+    async function saveCode(id, code) {
       let data = {[id]: code};
       const response = await fetch('https://algolab-c1646-default-rtdb.firebaseio.com/student_code.json', {
           method: 'PATCH',
@@ -57,7 +51,7 @@
           });
       const myJson = await response.json();
     }
-    const getCode = async function (id) {
+    async function getCode(id) {
       const response = await fetch(`https://algolab-c1646-default-rtdb.firebaseio.com/student_code/${id}.json`, {
           method: 'GET',
           headers: {
@@ -78,10 +72,8 @@
         redirect: 'follow',
       };
     
-      console.log("In fetchAnswer two-sum -> " + localStorage['problem']);
-      
-      let response;
-      let formattedResponse;
+      var response;
+      var formattedResponse;
       try {
         response = await fetch(
           `https://api.airtable.com/v0/appqasx2lkrlZ5e97/leetcode-solutions?filterByFormula=(%7Bproblem-url%7D+%3D+'${localStorage['problem']}')`,
@@ -101,8 +93,8 @@
     const codeTab = document.getElementById('edit-js');
     const cssTab = document.getElementById('edit-css');
     
-    const selectTab = function (tabId) {
-        const lastSelectedTab = document.getElementsByClassName('editor-tab--active');
+    function selectTab(tabId) {
+      const lastSelectedTab = document.getElementsByClassName('editor-tab--active');
       for (let i = 0; i < lastSelectedTab.length; i += 1) {
         lastSelectedTab[i].classList.remove('editor-tab--active');
       }
@@ -120,9 +112,6 @@
     } 
     
     language.addEventListener('change', (e) => {
-      
-      console.log(JSON.stringify(solutions));
-      
       js.setMode(`ace/mode/${e.target.value}`);
       css.setMode(`ace/mode/${e.target.value}`);
       
@@ -131,7 +120,6 @@
     
     codeTab.addEventListener('click', () => {
       selectTab('edit-js');
-      
       editor.setSession(js);
     });
     
@@ -140,4 +128,4 @@
       css.setValue(JSON.parse(localStorage[localStorage['problem']])["Python3"]);
       editor.setSession(css);
     });
-}();
+})();
